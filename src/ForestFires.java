629580a483cab2e4a8278d6f007031bd6e2a0371
RaphByrne <raph.byrne@gmail.com>
2013-05-27 15:57:50 +0800

    added convergence test for ForestFires.java now you don't need to specify a number of iterations it just stops once it converges

--- src/ForestFires.java
+++ src/ForestFires.java
@@ -1,3 +1,5 @@
+import java.lang.*;
+
 /**
  * Starter class for running the forest fires experiment
  */
@@ -7,6 +9,8 @@ public class ForestFires {
 		System.out.println(model.numEmpty + " " + model.numTrees + " " + model.numFires);
 	}
 
+
+
 	/**
  	* Runs the model for a number of iterations, prints data to standard out in form:
  	* iteration_num modelStats
@@ -14,18 +18,69 @@ public class ForestFires {
  	* @param model the model
  	* @param numIter the number of iterations to run
  	*/
-	private static void run(CAModel model, int numIter) {
+	private static void run(CAModel model) {
+		int minSteps = 20;
+		int[] lastEmpties = new int[minSteps];
+		int[] lastTrees = new int[minSteps];
+		int[] lastFires = new int[minSteps];
+		int i = 0;
+		System.out.print(i + " ");	
 		printStats(model);
-		for(int i = 1; i <= numIter; i++) {
+		for(i = 1; i < minSteps; i++) {
 			model.step();
 			System.out.print(i + " ");
 			printStats(model);
+			lastEmpties[i] = model.numEmpty;
+			lastTrees[i] = model.numTrees;
+			lastFires[i] = model.numFires;
 		}
-	}	
+		while(!convergenceCheck(lastEmpties,lastTrees,lastFires, (i-1)%minSteps)) {
+			model.step();
+			System.out.print(i + " ");
+			printStats(model);
+			int index = (i% minSteps);
+			lastEmpties[index] = model.numEmpty;
+			lastTrees[index] = model.numTrees;
+			lastFires[index] = model.numFires;
+			i++;
+		}	
+	}
+
+	private static double average(int[] a) {
+		int sum = 0;	
+		for(Integer i : a) {
+			sum += i;
+		}
+		return sum/a.length;
+	}
+
+	private static double tolerance = 0.01;
+
+	private static boolean convergenceCheck(int[] lastEmpties, int[] lastTrees, int[] lastFires, int i) {
+		int len = Math.max(lastEmpties.length, lastTrees.length);
+		len = Math.max(len, lastFires.length);
+		int lastE = lastEmpties[i];
+		int lastT = lastTrees[i]; 
+		int lastF = lastFires[i];
+		double aveE = average(lastEmpties);
+		double aveT = average(lastTrees);
+		double aveF = average(lastFires);
+		double varE = tolerance*aveE;
+		double varT = tolerance*aveT;
+		double varF = tolerance*aveF;
+		boolean result = true;
+		if(lastE > (aveE + varE) || lastE < (aveE - varE))
+			result = false;
+		if(lastT > (aveT + varT) || lastT < (aveT - varT))
+			result = false;
+		if(lastF > (aveF + varF) || lastF < (aveF - varF))
+			result = false;
+		return result;
+	}
 
 	public static void main(String[] args) {
-		if(args.length < 6) {
-			System.out.println("Arguments are: width height init_tree_chance growthrate firechance num_iterations");
+		if(args.length < 5) {
+			System.out.println("Arguments are: width height init_tree_chance growthrate firechance");
 			System.exit(0);
 		}
 		
@@ -35,10 +90,9 @@ public class ForestFires {
 		double q = Double.parseDouble(args[2]);
 		double growth = Double.parseDouble(args[3]);
 		double firechance = Double.parseDouble(args[4]);
-		int numIter = Integer.parseInt(args[5]);
 
 		CAModel model = new CAModel(width,height,q,growth,firechance);
-		run(model, numIter);
+		run(model);
 	}
 
 }
